1bcf1893496c27c645c06f09cefac811299db3de
ChristianoBourguignon <christianombl@gmail.com>
2025-03-28 14:50:41 -0300

    Inicialização do projeto

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import threads.TE;
+import threads.TI;
+import view.Janela;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
